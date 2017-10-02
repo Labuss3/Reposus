bae172156072799d234d4bbcaba41a42416f19fd
asus <b00066375@aus.edu>
2017-10-02 13:46:11 +0400

    Initial Commit

---
+++ Hello.java
@@ -0,0 +1,14 @@
+
+package hello;
+
+/**
+ *
+ * @author b00066375
+ */
+public class Hello {
+    
+    public static void main(String[] args) {
+        System.out.println("HELLO WOOOORRRLLLLLDDDD_USS !!!");
+    }
+    
+}
