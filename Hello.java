9dffead6039c4ce430ed8c49cc7e7f76153e2337
asus <b00066375@aus.edu>
2017-10-02 14:14:18 +0400

    Feature A added

--- Hello.java
+++ Hello.java
@@ -10,7 +10,9 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("HELLO WOOOORRRLLLLLDDDD_USS !!!");
     }
-    
+    public void addfeatureA(){
+        System.out.println("FEATURE A ADDED");
+    }
     void addfeatureB(){
         System.out.println("Feature B added!");
     }
